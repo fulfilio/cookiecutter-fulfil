2602716e73d94982a59c4ff966486a7a06307268
Sharoon Thomas <sharoon.thomas@fulfil.io>
2019-06-27 05:48:44 -0400

    Add REDIS and DATABASE URI to env.sh

--- {{cookiecutter.app_name}}/env.sh
+++ {{cookiecutter.app_name}}/env.sh
@@ -3,3 +3,6 @@ export FULFIL_APP_ID={{ cookiecutter.fulfil_app_id }}
 export FULFIL_APP_SECRET={{ cookiecutter.fulfil_app_secret }}
 export {{ cookiecutter.app_name|upper }}_SECRET_KEY={{ cookiecutter.secret_key }}
 export ENCR_KEY={{ cookiecutter.encryption_key }}
+
+export REDIS_URL=redis://localhost:6379/0
+export DATABASE_URI=postgres://{{ cookiecutter.app_name }}:{{ cookiecutter.app_name }}@localhost:5432/{{ cookiecutter.app_name }}
